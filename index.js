dfcb1c9e1768bfc275cd4362e9cb0c9950e2aa60
Masud <ranamasud0701@gmail.com>
2023-10-08 11:01:18 +0600

    added the server

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express();
+require('dotenv').config();
+const cors = require('cors');
+
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+
+app.get('/',(req,res)=>{
+     res.send('jewelry shop comming');
+})
+
+app.listen(port,()=>{
+     console.log(`jewelry server running on ${port}`);
+})
+
+
